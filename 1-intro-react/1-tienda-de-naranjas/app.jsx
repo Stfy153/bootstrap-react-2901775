ab96561f6ef2d42b04fa569a00ee0b67e95976b1
Stfy153 <169182693+Stfy153@users.noreply.github.com>
2024-10-23 07:36:15 -0500

    Creacion tiendas react

---
+++ 1-intro-react/1-tienda-de-naranjas/app.jsx
@@ -0,0 +1,12 @@
+function app () {
+    return(
+        <div>
+            <header>
+                <nav>menu de navegacion</nav>
+            </header>
+            <section>seccion promos</section>
+            <section>seccion tienda</section>
+            <footer>pie de pagina </footer>
+        </div>
+    )
+}
